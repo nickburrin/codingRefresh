0cd1a2867979ccbb13f0a57ffd5ab46859945eae
NickBurrin <nick.burrin@faithlife.com>
2019-07-21 15:30:04 -0700

    Solve jumping clouds problem

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 
@@ -27,21 +27,49 @@ public class Solution {
         return pairs;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader scanner = Files.newBufferedReader(Paths.get("input.txt"));
-        
-        int n = Integer.parseInt(scanner.readLine());
+    /**
+     * You're playing a new game that starts with consecutively numbered clouds. Some of the clouds are safe to jump on and others are not.
+     * You can jump on any safe clouds having a number that is equal to the number of the current cloud plus 1 or 2. 
+     * You must avoid the unsafe clouds. Determine the minimum number of jumps it will take to jump from the starting postion (clouds[0]) to the last cloud (clouds[n-1]).
+     * It is always possible to win the game.
+     * @param clouds - an array of consecutively numbered clouds. cloud[n] == 0 is safe, cloud[n] == 1 is unsafe
+     * @return the minimum number of moves required to traverse the clouds safely
+     */
+    static int jumpingOnClouds(int[] clouds) {
+        // I always know that clouds[0] and clouds[n-1] are safe
+        int i = 0;
 
-        int[] ar = new int[n];
-
-        String[] arItems = scanner.readLine().split(" ");
-
-        for (int i = 0; i < n; i++) {
-            int arItem = Integer.parseInt(arItems[i]);
-            ar[i] = arItem;
+        int minJumps = 0;
+        while (i < clouds.length - 2) {
+            if (clouds[i + 2] == 0)
+                i += 2;
+            else
+                i += 1;
+            
+            minJumps++;
         }
 
-        System.out.println(sockMerchant(n, ar));
+        if (i != clouds.length - 1)
+            minJumps++;
+
+        return minJumps;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(new File(args[0]));
+        
+        while (scanner.hasNext()){
+            int n = Integer.parseInt(scanner.nextLine());
+            int[] clouds = new int[n];
+
+            String[] arItems = scanner.nextLine().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                clouds[i] = Integer.parseInt(arItems[i]);
+            }
+
+            System.out.println("Minimum number of moves is: " + jumpingOnClouds(clouds));
+        }
 
         scanner.close();
     }
