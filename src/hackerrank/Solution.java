81012ea121505881891b0747e6e78e1c34dbde76
NickBurrin <nick.burrin@faithlife.com>
2019-08-08 15:04:03 -0700

    Adds Calculator class to add sum of divisors

--- src/hackerrank/Solution.java
+++ src/hackerrank/Solution.java
@@ -251,33 +251,9 @@ class Solution {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
-        String input = scan.nextLine();
-        scan.close();
-
-        // Convert input String to an array of characters:
-        char[] s = input.toCharArray();
-
-        // Create a Solution object:
-        Solution p = new Solution();
-
-        // Enqueue/Push all chars to their respective data structures:
-        for (char c : s) {
-            p.pushCharacter(c);
-            p.enqueueCharacter(c);
-        }
-
-        // Pop/Dequeue the chars at the head of both data structures and compare them:
-        boolean isPalindrome = true;
-        for (int i = 0; i < s.length/2; i++) {
-            if (p.popCharacter() != p.dequeueCharacter()) {
-                isPalindrome = false;                
-                break;
-            }
-        }
-
-        //Finally, print whether string s is palindrome or not.
-        System.out.println( "The word, " + input + ", is " 
-                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+        int input = scan.nextInt();
+        Calculator calc = new Calculator();
+        System.out.println("Sum of divisors of " + input + " is " + calc.divisorSum(input));
         
         scan.close();
     }
